c1f90f832973c09f4778c6e06367f3c17f03813a
Joao-J <tenata@debian-BULLSEYE-live-builder-AMD64>
2023-03-05 03:47:59 -0300

    testando ymal

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
 import yaml
+
+bd = []
+
+class banco_dados:
+
+	global bd
+
+	def importar():
+		try:
+			with open('bd.yml','r') as file:
+				bd = yaml.safe_load(file)
+		except:
+			open('bd.yml','x')
+		print('banco de dados: OK')
+
+	def update(value):
+		bd.append(value)
+		print(bd)
+		with open('bd.yml','w') as file:
+			yaml.dump(bd,file)
+		print('banco de dados: Atualizado')
+		return(bd)
+
+
+class main:
+	def __init__(self):
+		b = banco_dados
+		b.importar()
+		print(b.update({'Nome':'maria','Quantidade':2,'Valor':2.80}))
+
+
+main()
